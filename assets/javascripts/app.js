dbed06ab211fcaef4f84ec6c5fee787cb7e9dec2
Andrew <re.admit.re@gmail.com>
2013-11-21 13:19:28 +0400

    Added index template content

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -7,6 +7,7 @@ define(['jquery',
        'routers/nav'],
   function($, _, Backbone, Bootstrap, NavRouter) {
   	$('body > .container').height( $(window).height() - 90 )
+  	$('#contacts').popover()
   	var navigation = new NavRouter.Nav()
   		Backbone.history.start()
   }
