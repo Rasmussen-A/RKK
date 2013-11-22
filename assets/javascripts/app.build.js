e4ca118797d1575a5f80ba8c35ea488ebd31e994
Andrew <re.admit.re@gmail.com>
2013-11-22 14:57:28 +0400

    Added script for full deployment

---
+++ assets/javascripts/app.build.js
@@ -0,0 +1,17 @@
+({
+    appDir: "../../",
+    baseUrl: "assets/javascripts/",
+    dir: "../../../RKK-Deploy",
+    paths: {
+        jquery: "empty:",
+        underscore: "empty:",
+        backbone: "empty:",
+        bootstrap: "empty:",
+        templates: "../templates"
+    },
+    modules: [
+        {
+            name: "main"
+        }
+    ]
+})
